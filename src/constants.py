9cf17a7e8c8a5c6dacadd6cbd3d1fb68620c0936
peulsilva <peulsilva9@gmail.com>
2024-04-07 14:34:18 +0200

    add data analysis and constants

---
+++ src/constants.py
@@ -0,0 +1,5 @@
+import os
+
+CLEANED_HOUSEHOLD_POWER_PATH = os.path.join("data", "household_power_consumption", "cleaned_household_power.pkl") 
+CLEANED_WEATHER_PATH = os.path.join("data", "weather", "cleaned_weather.pkl")
+HOUSEHOLD_WEATHER_MERGED_PATH = os.path.join("data", "preprocessed", "household_consumption.pkl")
